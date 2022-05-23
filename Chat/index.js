7e0b056bc890a905957040a69666566f82f9cf81
bram.silue <n/a>
2022-05-23 18:53:34 +0200

    re-added encryption functions on server side

--- Chat/index.js
+++ Chat/index.js
@@ -33,6 +33,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const crypto = require('crypto');
 
+// AES encryption
+function aesEncrypt (to_encrypt_, key_, iv_) {
+  let to_encrypt = Buffer.from(to_encrypt_, 'utf8');
+  let key = Buffer.from(key_, 'hex');
+  let iv = Buffer.from(iv_, 'hex');
+
+  const cipher = crypto.createCipheriv('aes-256-ctr', key, iv);
+  let encrypted = cipher.update(to_encrypt, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+
+  return encrypted;
+};
+
+// AES decryption
+function aesDecrypt (encrypted_, key_, iv_) {
+  let encrypted = Buffer.from(encrypted_, 'hex');
+  let key = Buffer.from(key_, 'hex');
+  let iv = Buffer.from(iv_, 'hex');
+
+  const decipher = crypto.createDecipheriv('aes-256-ctr', key, iv);
+  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+
+  return decrypted;
+};
+
+// PBKDF2 Hashing
 function pbkdf2(password) {
   const salt = crypto.randomBytes(60); 
   let key = crypto.pbkdf2Sync(password, salt, 100000, 64, 'sha512');
