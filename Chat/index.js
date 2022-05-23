e9f6113a52e38a964ee68b31e2249834efb24705
bram.silue <n/a>
2022-05-23 14:22:17 +0200

    added pbkdf2 function for hashing passwords

--- Chat/index.js
+++ Chat/index.js
@@ -33,29 +33,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const crypto = require('crypto');
 
-function aesEncrypt (to_encrypt_, key_, iv_) {
-  let to_encrypt = Buffer.from(to_encrypt_, 'utf8');
-  let key = Buffer.from(key_, 'hex');
-  let iv = Buffer.from(iv_, 'hex');
+function pbkdf2(password) {
+  const salt = crypto.randomBytes(60); 
+  let key = crypto.pbkdf2Sync(password, salt, 100000, 64, 'sha512');
 
-  const cipher = crypto.createCipheriv('aes-256-ctr', key, iv);
-  let encrypted = cipher.update(to_encrypt, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
-
-  return encrypted;
-};
-
-function aesDecrypt (encrypted_, key_, iv_) {
-  let encrypted = Buffer.from(encrypted_, 'hex');
-  let key = Buffer.from(key_, 'hex');
-  let iv = Buffer.from(iv_, 'hex');
-
-  const decipher = crypto.createDecipheriv('aes-256-ctr', key, iv);
-  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
-
-  return decrypted;
-};
+  return {key: key, salt: salt};
+}
 
 
 ///////////////////////////////
@@ -186,16 +169,6 @@ io.on('connection', (socket) => {
   socket.on('new message', (msg) => {
     if (userLoggedIn) {
       console.log(msg);
-
-      /*
-      let key = "1234567890123456789012345678901212345678901234567890123456789012";
-      let iv = "12345678901234561234567890123456";
-
-      let encrypted = encrypt("ttt", key, iv);
-      console.log(encrypted);
-      let decrypted = decrypt(msg.message, key, iv);
-      console.log(decrypted);
-      */
 
       addMessageToRoom(msg.room, username, msg);
     }
